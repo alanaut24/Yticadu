521b8fa621f01852daf5b19e3448a9b299c476be
alanaut24 <alnhurt24@gmail.com>
2017-03-17 03:35:04 -0500

    Added Directories, notes files, sub-directories

---
+++ Courses/Intro to Java/L1 Solutions/question1.java
@@ -0,0 +1,18 @@
+/*
+Question 1:
+Which of the following Java variable declarations has an error?
+*/
+
+A.	int x = 5;
+B.	double temperature = 75.6;
+C.	char grade = ’A’;
+D.	String name = ’Adam’;
+
+/*
+
+Answer = D.
+
+When deﬁning Strings, we need to use double quotes around the text, like this: String name = "Adam";
+Single quotes are used for deﬁning single characters, like this:
+char grade = ’A’;
+*/
